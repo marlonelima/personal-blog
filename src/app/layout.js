a1429239c4dd43bd74139fc5426a9bed3f23b8cc
Marlone Lima <marlonelimabr@gmail.com>
2025-05-10 21:50:10 -0300

    Fix repository-name

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,31 +26,6 @@ export default function RootLayout({ children }) {
     >
       <body className="overflow-x-hidden antialiased">
         <main>
-          {process.env.NODE_ENV === "development" && (
-            <div
-              style={{
-                background: "#5163ba",
-                padding: "1rem",
-                textAlign: "center",
-                fontSize: "0.85rem",
-                color: "#fff",
-              }}
-            >
-              <p>
-                <strong>👋 Welcome to your new website!</strong> To customize
-                the code and content of this site,{" "}
-                <a
-                  href="https://github.com/prismicio-community/nextjs-starter-prismic-blog/tree/master/docs"
-                  target="_blank"
-                  rel="noreferrer"
-                  style={{ textDecoration: "underline" }}
-                >
-                  see the documentation
-                </a>
-                . Remove this bar in <code>app/layout.js</code>.
-              </p>
-            </div>
-          )}
           {children}
           <PrismicPreview repositoryName={repositoryName} />
         </main>
